d1a7cecb14e62cb036a779d5bf96cd20e486690d
Lautaro Mascherano <Laumascheranox@outlook.com>
2022-10-25 11:39:51 -0300

    commit

--- BestWallet/src/app/app.module.ts
+++ BestWallet/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HeaderComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
