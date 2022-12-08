50111ae69a235d333cb783fa9a2f898781ed5652
candelariaSobol <candee_sobol@hotmail.com>
2022-12-08 12:23:24 -0300

    Creacion del nuevo proyecto

---
+++ CryptoAPI2/CryptoAPI2/Models/Moneda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoAPI2.Models
+{
+    public partial class Moneda
+    {
+        public Moneda()
+        {
+            Cuenta = new HashSet<Cuenta>();
+        }
+
+        public int IdMoneda { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string? Codigo { get; set; }
+
+        public virtual ICollection<Cuenta> Cuenta { get; set; }
+    }
+}
---
+++ CryptoAPI2/CryptoAPI2/Models/Transaccione.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoAPI2.Models
+{
+    public partial class Transaccione
+    {
+        public int IdTransaccion { get; set; }
+        public int IdTipoTransaccion { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+        public int IdCuenta { get; set; }
+
+        public virtual Cuenta IdCuentaNavigation { get; set; } = null!;
+        public virtual TipoTransaccion IdTipoTransaccionNavigation { get; set; } = null!;
+    }
+}
