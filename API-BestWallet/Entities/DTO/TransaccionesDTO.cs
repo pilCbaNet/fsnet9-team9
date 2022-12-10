9782d634b0e23d5a3677b8abc7b22a9f9e58d413
candelariaSobol <candee_sobol@hotmail.com>
2022-12-10 16:11:43 -0300

    POST deposito y retiro

---
+++ API-BestWallet/Entities/DTO/TransaccionesDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class TransaccionesDTO
+    {
+        //public int IdTipoTransaccion { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+        public int IdCuenta { get; set; }
+    }
+}
